861101b2d5b6d96e9f482c0f80bd88195520d871
Paul Rail <paul.a.rail@gmail.com>
2017-09-28 22:05:58 -0400

    Finished this course for now

--- 3rd script.js
+++ 3rd script.js
@@ -55,3 +55,9 @@ $(function() {
     }
   });
 });
+
+
+//DRAGGABLE WITH JQUERY UI
+
+//requires the CDN script inthe HTML header:
+//<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
---
+++ 4th script.js
@@ -0,0 +1,9 @@
+
+//DRAGGABLE WITH JQUERY UI
+
+//requires the CDN script inthe HTML header:
+//<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+$(function() {
+  $("#draggable").draggable();
+});
